86dd81528f8bc6fd476ccd127f39cd75667361e3
Ilona Budapesti <ilona.budapesti@gmail.com>
2017-11-07 23:39:27 +0000

    add tipiṭaka whole corpus in xml and basic stats on the whole corpus

---
+++ Thesis_Oxford_MSt/DPR_Myanmar_xml_and_processing/processed/corpus.py
@@ -0,0 +1,36 @@
+from nltk.corpus import PlaintextCorpusReader
+corpus_root = '/Dropbox/repos/digitalpalireader/DPRMyanmar/content/xml'
+
+
+
+fileid[0]
+
+genres = {
+  'v': 'Vinaya',
+  'd': 'Dīgha'
+  'm': 'Majjhima',
+  's': 'Saṃyutta'
+  'a': 'Aṅguttara',
+  'k': 'Khuddaka',
+  '?': 'Abhidhamma',
+  'x': 'Vism',
+  '?': 'Abhidh-s',
+  '?': 'Byākaraṇa',
+  'b': ?,
+  'g': ?,
+
+}
+
+fileid[-1]
+
+level = {
+  'm': 'Mūla',
+  'a': 'Aṭṭhakathā'
+  't': 'ṭīka'
+}
+
+baskets = {
+  'vinayapiṭaka': ['v'],
+  'suttapiṭaka': ['d', 's', 'a', 'm'],
+  'abhidammapiṭaka': ['?', '?']
+}
